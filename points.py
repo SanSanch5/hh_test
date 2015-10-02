fcb3e5637830f859bf00d8e018b687baba4e85f9
sansanch5 <a.a.pakhomoff@gmail.com>
2015-10-02 14:48:16 +0300

    [added] nearest point search

--- points.py
+++ points.py
@@ -1,18 +1,8 @@
 from scipy import spatial
 
-x, y, z = np.mgrid[0:5, 2:8, 2:3]
-data = zip(x.ravel(), y.ravel(), z.ravel())
+data = [[1,1], [2,1], [4,1], [5,2], [1,3], [3,2]]
+
 tree = spatial.KDTree(data)
-print 'ball', [data[i] for i in tree.query_ball_point(array([1,2,2]), 1)]
-distance, index = tree.query(np.array([[2, 2, 2.2]]))
-print 'query', distance, index, data[index[0]]
-pts = np.array([[2, 2, 2.2]])
-
-tree.query(pts)
-
-import heapq
-def euclideanDistance(x,y):
-    return math.sqrt(sum([(a-b)**2 for (a,b) in zip(x,y)]))
-closestPoints = heapq.nsmallest(1, enumerate(pts), key=lambda y: euclideanDistance(x, y[1]))
-
-print closestPoints
+# print('ball', [data[i] for i in tree.query_ball_point([2,1], 1)])
+distance, index = tree.query([2, 5])
+print('query', distance, index, data[index])
